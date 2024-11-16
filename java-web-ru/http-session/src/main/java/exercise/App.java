485936c93afa66908c951b3fecd197f8be5088f8
@hexlet/cli <support@hexlet.io>
2024-11-16 18:20:14 +0300

    submit java-web-ru/http-session

--- java-web-ru/http-session/src/main/java/exercise/App.java
+++ java-web-ru/http-session/src/main/java/exercise/App.java
@@ -1,12 +1,15 @@
 package exercise;
 
 import io.javalin.Javalin;
+import io.javalin.http.Context;
 import java.util.List;
 import java.util.Map;
 
 public final class App {
 
     private static final List<Map<String, String>> USERS = Data.getUsers();
+    private static final int DEFAULT_PAGE = 1;
+    private static final int DEFAULT_PER_PAGE = 5;
 
     public static Javalin getApp() {
 
@@ -15,23 +18,29 @@ public final class App {
         });
 
         // BEGIN
-        app.get("/users", ctx -> {
-            int pageNum = ctx.queryParamAsClass("page", Integer.class)
-                    .getOrDefault(1);
-            int resultsNum = ctx.queryParamAsClass("per", Integer.class)
-                    .getOrDefault(5);
-            int end = pageNum * resultsNum;
-            int begin = pageNum * resultsNum - resultsNum;
-            ctx.json(USERS.subList(begin, end));
-        });
+        app.get("/users", App::handleGetUsers);
         // END
 
         return app;
+    }
 
+    private static void handleGetUsers(Context ctx) {
+        int page = ctx.queryParam("page") != null ? Integer.parseInt(ctx.queryParam("page")) : DEFAULT_PAGE;
+        int perPage = ctx.queryParam("per") != null ? Integer.parseInt(ctx.queryParam("per")) : DEFAULT_PER_PAGE;
+
+        int fromIndex = (page - 1) * perPage;
+        int toIndex = Math.min(fromIndex + perPage, USERS.size());
+
+        if (fromIndex >= USERS.size()) {
+            ctx.json(List.of());
+        } else {
+            List<Map<String, String>> usersPage = USERS.subList(fromIndex, toIndex);
+            ctx.json(usersPage);
+        }
     }
 
     public static void main(String[] args) {
         Javalin app = getApp();
         app.start(7070);
     }
-}
+}}
