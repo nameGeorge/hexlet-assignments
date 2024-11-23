3150548551e6a8335dabd23998107708e071ee13
@hexlet/cli <support@hexlet.io>
2024-11-23 21:55:06 +0300

    submit java-web-ru/flash

--- java-web-ru/flash/src/main/java/exercise/dto/BasePage.java
+++ java-web-ru/flash/src/main/java/exercise/dto/BasePage.java
@@ -9,7 +9,9 @@ public class BasePage {
     private String flash;
     private String flashType;
 
-    public BasePage() {}
+    public BasePage() {
+
+    }
 
     public BasePage(String flash, String flashType) {
         this.flash = flash;
