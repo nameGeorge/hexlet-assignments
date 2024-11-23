bfe3dd217f9ab741dd159af8d202067208fe63ce
@hexlet/cli <support@hexlet.io>
2024-11-23 21:19:46 +0300

    submit java-web-ru/flash

--- java-web-ru/flash/src/main/java/exercise/dto/BasePage.java
+++ java-web-ru/flash/src/main/java/exercise/dto/BasePage.java
@@ -1,18 +1,15 @@
 package exercise.dto;
 
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BasePage {
     private String flash;
-    private String flashType;
-
-    public BasePage() {}
-
-    public BasePage(String flash, String flashType) {
-        this.flash = flash;
-        this.flashType = flashType;
-    }
 }
