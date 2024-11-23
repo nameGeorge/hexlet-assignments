0845e17d9192d2c18608decb69aecf07731cd938
@hexlet/cli <support@hexlet.io>
2024-11-24 00:38:53 +0300

    submit java-web-ru/flash

--- java-web-ru/flash/src/main/java/exercise/controller/PostsController.java
+++ java-web-ru/flash/src/main/java/exercise/controller/PostsController.java
@@ -1,51 +1,51 @@
 package exercise.controller;
 
-import exercise.dto.posts.BuildPostPage;
-import exercise.dto.posts.PostPage;
+import java.util.Collections;
+
 import exercise.dto.posts.PostsPage;
+import exercise.dto.posts.PostPage;
 import exercise.model.Post;
 import exercise.repository.PostRepository;
+import exercise.dto.posts.BuildPostPage;
 import exercise.util.NamedRoutes;
 import io.javalin.http.Context;
 import io.javalin.validation.ValidationException;
 import io.javalin.http.NotFoundResponse;
 
-import static io.javalin.rendering.template.TemplateUtil.model;
-
 public class PostsController {
 
     public static void build(Context ctx) {
         var page = new BuildPostPage();
-        ctx.render("posts/build.jte", model("page", page));
+        ctx.render("posts/build.jte", Collections.singletonMap("page", page));
     }
 
+    // BEGIN
     public static void create(Context ctx) {
         try {
             var name = ctx.formParamAsClass("name", String.class)
-                    .check(value -> value.length() >= 2, "Название не должно быть короче двух символов")
+                    .check(value -> value.length() >= 2, "Название поста слишком короткое")
                     .get();
             var body = ctx.formParam("body");
             var post = new Post(name, body);
             PostRepository.save(post);
             ctx.sessionAttribute("flash", "Пост был успешно создан!");
-            ctx.sessionAttribute("flash-type", "success");
             ctx.redirect(NamedRoutes.postsPath());
-
         } catch (ValidationException e) {
             var name = ctx.formParam("name");
             var body = ctx.formParam("body");
             var page = new BuildPostPage(name, body, e.getErrors());
-            ctx.render("posts/build.jte", model("page", page)).status(422);
+            ctx.render("posts/build.jte", Collections.singletonMap("page", page));
         }
     }
 
     public static void index(Context ctx) {
         var posts = PostRepository.getEntities();
+        String flash = ctx.consumeSessionAttribute("flash");
         var page = new PostsPage(posts);
-        page.setFlash(ctx.consumeSessionAttribute("flash"));
-        page.setFlashType(ctx.consumeSessionAttribute("flash-type"));
-        ctx.render("posts/index.jte", model("page", page));
+        page.setFlash(flash);
+        ctx.render("posts/index.jte", Collections.singletonMap("page", page));
     }
+    // END
 
     public static void show(Context ctx) {
         var id = ctx.pathParamAsClass("id", Long.class).get();
@@ -53,6 +53,6 @@ public class PostsController {
                 .orElseThrow(() -> new NotFoundResponse("Post not found"));
 
         var page = new PostPage(post);
-        ctx.render("posts/show.jte", model("page", page));
+        ctx.render("posts/show.jte", Collections.singletonMap("page", page));
     }
 }
--- java-web-ru/flash/src/main/java/exercise/dto/BasePage.java
+++ java-web-ru/flash/src/main/java/exercise/dto/BasePage.java
@@ -7,13 +7,4 @@ import lombok.Setter;
 @Setter
 public class BasePage {
     private String flash;
-    private String flashType;
-
-    public BasePage() {
-    }
-
-    public BasePage(String flash, String flashType) {
-        this.flash = flash;
-        this.flashType = flashType;
-    }
 }
