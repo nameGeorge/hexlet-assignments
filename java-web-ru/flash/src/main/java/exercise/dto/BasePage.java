48fdcf4d64999a79755038fb8b95e007981735d5
@hexlet/cli <support@hexlet.io>
2024-11-23 22:53:00 +0300

    submit java-web-ru/flash

--- java-web-ru/flash/src/main/java/exercise/dto/BasePage.java
+++ java-web-ru/flash/src/main/java/exercise/dto/BasePage.java
@@ -8,8 +8,14 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class BasePage {
     private String flash;
+    private String flashType;
+
+    public BasePage() {}
+
+    public BasePage(String flash, String flashType) {
+        this.flash = flash;
+        this.flashType = flashType;
+    }
 }
--- java-web-ru/flash/src/main/java/exercise/dto/posts/PostsPage.java
+++ java-web-ru/flash/src/main/java/exercise/dto/posts/PostsPage.java
@@ -11,10 +11,17 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 public class PostsPage extends BasePage {
-
     private List<Post> posts;
 
+    public PostsPage(List<Post> posts) {
+        super();
+        this.posts = posts;
+    }
+
+    public PostsPage(List<Post> posts, String flash, String flashType) {
+        super(flash, flashType);
+        this.posts = posts;
+    }
 }
