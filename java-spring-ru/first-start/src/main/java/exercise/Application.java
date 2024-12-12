a90fb1f9f863cefd68ca4cb8d7280afee6c42766
@hexlet/cli <support@hexlet.io>
2024-12-12 21:57:42 +0300

    submit java-spring-ru/first-start

--- java-spring-ru/first-start/src/main/java/exercise/Application.java
+++ java-spring-ru/first-start/src/main/java/exercise/Application.java
@@ -14,7 +14,7 @@ public class Application {
     }
 
     @GetMapping("/about")
-    String about() {
-        return "Welcome to Hexlet !";
+    String home() {
+        return "Welcome to Hexlet!";
     }
 }
