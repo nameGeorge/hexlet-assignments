a9feefaf14d430d066f4f55197fdccdd833141ca
@hexlet/cli <support@hexlet.io>
2024-12-20 22:09:45 +0300

    submit java-spring-ru/jpa-comparing-entities

--- java-spring-ru/jpa-comparing-entities/src/main/java/exercise/model/Product.java
+++ java-spring-ru/jpa-comparing-entities/src/main/java/exercise/model/Product.java
@@ -1,15 +1,29 @@
 package exercise.model;
 
-import jakarta.persistence.Id;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import static jakarta.persistence.GenerationType.IDENTITY;
-
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-// BEGIN
+import static jakarta.persistence.GenerationType.IDENTITY;
 
+// BEGIN
+@Entity
+@Table
+@Getter
+@Setter
+@EqualsAndHashCode(of = {"title", "price"})
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private long id;
+
+    private String title;
+
+    private int price;
+}
 // END
