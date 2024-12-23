54c01f2814a96c784135dec1ccd07d56d47d4277
@hexlet/cli <support@hexlet.io>
2024-12-23 21:49:37 +0300

    submit java-spring-boot-ru/dependency-injection

--- java-spring-boot-ru/dependency-injection/src/main/java/exercise/controller/PostsController.java
+++ java-spring-boot-ru/dependency-injection/src/main/java/exercise/controller/PostsController.java
@@ -2,7 +2,15 @@ package exercise.controller;
 
 import exercise.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.http.HttpStatus;
 import java.util.List;
 
@@ -11,60 +19,46 @@ import exercise.repository.PostRepository;
 import exercise.exception.ResourceNotFoundException;
 
 // BEGIN
+
 @RestController
 @RequestMapping("/posts")
 public class PostsController {
 
-    private CommentRepository commentRepository;
+    @Autowired
     private PostRepository postRepository;
 
-    public PostsController(CommentRepository commentRepository, PostRepository postRepository) {
-        this.commentRepository = commentRepository;
-        this.postRepository = postRepository;
-    }
+    @Autowired
+    private CommentRepository commentRepository;
 
-    @GetMapping
-    @ResponseStatus(HttpStatus.OK)
-    public List<Post> getPosts() {
+    @GetMapping("")
+    public List<Post> index() {
         return postRepository.findAll();
     }
 
-    @GetMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public Post getPost(@PathVariable long id) {
-        return postRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Post with id " + id + " not found"));
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Post create(@RequestBody Post post) {
+        return postRepository.save(post);
     }
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Post create(@RequestBody Post postDate) {
-        List<Post> posts = postRepository.findAll();
-        var post = posts.stream().filter(p -> p.equals(postDate)).findFirst();
-        if(post.isPresent()) {
-            throw new RuntimeException("Post already exists with id " + post.get().getId());
-        } else {
-            return postRepository.saveAndFlush(postDate);
-        }
+    @GetMapping("/{id}")
+    public Post show(@PathVariable long id) {
+        return postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post with id " + id + " not found"));
     }
 
     @PutMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public Post update(@RequestBody Post postData, @PathVariable long id) {
-        var post = postRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Post with id " + id + " not found"));
-        post.setBody(postData.getBody());
-        post.setTitle(postData.getTitle());
-        postRepository.saveAndFlush(post);
-        return post;
+    public Post update(@RequestBody Post post, @PathVariable long id) {
+        return postRepository.findById(id).map(p -> {
+            p.setTitle(post.getTitle());
+            p.setBody(post.getBody());
+            return postRepository.save(p);
+        }).orElseThrow(() -> new ResourceNotFoundException(id + "not found"));
     }
 
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public void delete(@PathVariable long id) {
-        postRepository.deleteById(id);
+    public void destroy(@PathVariable long id) {
         commentRepository.deleteByPostId(id);
+        postRepository.deleteById(id);
     }
-
 }
 // END
