caecd5e91205d2c2e5604b061156da5859d33a7e
@hexlet/cli <support@hexlet.io>
2024-12-24 00:50:59 +0300

    submit java-spring-boot-ru/dependency-injection

--- java-spring-boot-ru/dependency-injection/src/main/java/exercise/controller/PostsController.java
+++ java-spring-boot-ru/dependency-injection/src/main/java/exercise/controller/PostsController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.http.HttpStatus;
 import java.util.List;
-
 import exercise.model.Post;
 import exercise.repository.PostRepository;
 import exercise.exception.ResourceNotFoundException;
