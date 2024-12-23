0c632269a7b34fe0e0b6f2856fd665935ebd692a
@hexlet/cli <support@hexlet.io>
2024-12-23 23:07:04 +0300

    submit java-spring-boot-ru/dependency-injection_23.12.24_22.51.45

--- java-spring-boot-ru/dependency-injection_23.12.24_22.51.45/src/main/java/exercise/controller/CommentsController.java
+++ java-spring-boot-ru/dependency-injection_23.12.24_22.51.45/src/main/java/exercise/controller/CommentsController.java
@@ -1,15 +1,9 @@
 package exercise.controller;
 
+import exercise.model.Post;
+import exercise.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 
 import java.util.List;
@@ -19,5 +13,56 @@ import exercise.repository.CommentRepository;
 import exercise.exception.ResourceNotFoundException;
 
 // BEGIN
+@RestController
+@RequestMapping("/comments")
+public class CommentsController {
 
+    private CommentRepository commentRepository;
+
+    public CommentsController(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    @GetMapping
+    public List<Comment> getComments() {
+        return commentRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Comment getComment(@PathVariable long id) {
+        return commentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Comment with id " + id + " not found"));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Comment create(@RequestBody Comment commentData) {
+        List<Comment> comments = commentRepository.findAll();
+        var comment = comments.stream().filter(p -> p.equals(commentData)).findFirst();
+        if(comment.isPresent()) {
+            throw new RuntimeException("Comment already exists with id " + comment.get().getId());
+        } else {
+            return commentRepository.saveAndFlush(commentData);
+        }
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Comment update(@RequestBody Comment commentData, @PathVariable long id) {
+        var comment = commentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Post with id " + id + " not found"));
+        comment.setBody(commentData.getBody());
+        commentRepository.saveAndFlush(comment);
+        return comment;
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@PathVariable long id) {
+        commentRepository.deleteById(id);
+    }
+
+
+}
 // END
