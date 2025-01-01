cb80f237af8c22420e565788d8c28a5e81b9021b
@hexlet/cli <support@hexlet.io>
2025-01-01 20:54:43 +0300

    submit java-spring-boot-ru/dto-to-entity

---
+++ java-spring-boot-ru/dto-to-entity/src/main/java/exercise/controller/ContactsController.java
@@ -0,0 +1,51 @@
+package exercise.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import exercise.model.Contact;
+import exercise.repository.ContactRepository;
+import exercise.dto.ContactDTO;
+import exercise.dto.ContactCreateDTO;
+
+@RestController
+@RequestMapping("/contacts")
+public class ContactsController {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    // BEGIN
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ContactDTO create(@RequestBody ContactCreateDTO ccd) {
+        Contact contact = toContact(ccd);
+        Contact contactForDTO = contactRepository.save(contact);
+        return toContactDTO(contactForDTO);
+    }
+
+    private Contact toContact(ContactCreateDTO ccd) {
+        Contact contact = new Contact();
+        contact.setFirstName(ccd.getFirstName());
+        contact.setLastName(ccd.getLastName());
+        contact.setPhone(ccd.getPhone());
+        return contact;
+    }
+
+    private ContactDTO toContactDTO(Contact contact) {
+        ContactDTO contactDTO = new ContactDTO();
+        contactDTO.setId(contact.getId());
+        contactDTO.setFirstName(contact.getFirstName());
+        contactDTO.setLastName(contact.getLastName());
+        contactDTO.setPhone(contact.getPhone());
+        contactDTO.setCreatedAt(contact.getCreatedAt());
+        contactDTO.setUpdatedAt(contact.getUpdatedAt());
+        return contactDTO;
+    }
+    // END
+}
