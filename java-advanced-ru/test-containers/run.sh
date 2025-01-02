279dd95d0ec9f5f483b4c321280aded4feaa714b
@hexlet/cli <support@hexlet.io>
2025-01-03 00:25:28 +0300

    submit java-advanced-ru/test-containers

--- java-advanced-ru/test-containers/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -jar test-0.0.1-SNAPSHOT.jar
--- java-advanced-ru/test-containers/src/main/java/com/bismark/test/com/bismark/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bismark.test.com.bismark;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-
-}
